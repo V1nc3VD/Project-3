8f47088ea5b9bda96dca48d86dd04133d7bb8c20
Vince van Doorn <328388@student.mboutrecht.nl>
2020-02-20 23:53:46 +0100

    initial commit

---
+++ home.php
@@ -0,0 +1,31 @@
+<h2 class="text-center">
+    Welkom op onze website over dyslexie!
+</h2>
+
+<h3 class="hometext">
+    Hier kan u informatie vinden over dyslexie. Ook kan je hier een korte dyslexie test doen en een leuke game over dyslexie spelen!
+</h3>
+
+<div class="container-fluid">
+    <div class="row homecards">
+        <div class="card col-12 col-md-3">
+            <img class="card-img-top" src="./img/dummy1" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Informatie</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+            </div>
+        </div>
+        <div class="card col-12 offset-md-1 col-md-3">
+            <img class="card-img-top" src="./img/dummy2" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">Informatie</h5>
+            </div>
+        </div>
+        <div class="card col-12 offset-md-1 col-md-3">
+            <img class="card-img-top" src="./img/dummy3" alt="Card image cap">
+            <div class="card-body">Basic card</div>
+            <a href="#" class="btn btn-primary" onclick="dyslexiemodus()">Button</a>
+        </div>
+    </div>
+</div>
