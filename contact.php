8d63087f65ba2714af7c4a1277ad5e805def44d9
Vince van Doorn <323888>
2020-03-31 02:17:39 +0200

    contactformulier toegevoegd en werkend + mogelijkheid om als gast een bericht te sturen

---
+++ contact.php
@@ -0,0 +1,20 @@
+    <div class="row">
+        <div class="col-md-8 registreerform">
+            <h3 class="registreren text-center">Contact</h2>
+                <hr>
+                <p><b>Heeft u vragen, voel je dan vrij om hier een bericht achter te laten. We streven ernaar om binnen 24 uur een antwoord te geven.</b></p>
+                <form action="./index.php?content=contact_script" method="post">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Email</label>
+                        <input type="email" class="form-control registreerinput" id="email" name="email" aria-describedby="email" placeholder="">
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">Uw bericht</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" id="bericht" name="bericht" rows="7"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-primary submitregister">Submit</button>
+                </form>
+        </div>
+    </div>
+
+
+
