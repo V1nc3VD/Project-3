c7da404be1225b25775dcf258c01e42393623cfb
Vince van Doorn <323888>
2020-03-31 02:36:19 +0200

    "(optioneel)" tekst bij contact email wanneer ingelogd

--- contact.php
+++ contact.php
@@ -5,7 +5,14 @@
                 <p><b>Heeft u vragen, voel je dan vrij om hier een bericht achter te laten. We streven ernaar om binnen 24 uur een antwoord te geven.</b></p>
                 <form action="./index.php?content=contact_script" method="post">
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email</label>
+                        <label for="exampleInputEmail1">Email 
+                        <?php
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true)
+{
+    print(" (optioneel)");
+}
+?>
+                        </label>
                         <input type="email" class="form-control registreerinput" id="email" name="email" aria-describedby="email" placeholder="">
                         <div class="form-group">
                             <label for="exampleFormControlTextarea1">Uw bericht</label>
